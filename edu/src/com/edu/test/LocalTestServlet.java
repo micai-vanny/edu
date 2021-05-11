f4bd2e08083822a3b58c32069d59b42819189711
admin <admin@YD02-15>
2021-05-11 17:09:04 +0900

    2021 Servlet 수업 오늘자 최종최종

--- edu/src/com/edu/test/LocalTestServlet.java
+++ edu/src/com/edu/test/LocalTestServlet.java
@@ -11,10 +11,12 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/local")
 public class LocalTestServlet extends HttpServlet{
-	int number = 0;
+	int number = 0; 	// 멤버 변수로 선언하면 여러 클라이언트가 쓰레드를 공유하여 사용하게 됨.
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		//int number = 0;	// 지역변수로 선언하게 되면 클라이언트들이 각각의 쓰레드를 사용하게 된다.
+		
 		String str = req.getParameter("msg");
 		
 		resp.setContentType("text/html;charset=UTF-8");
